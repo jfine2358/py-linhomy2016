d33bf710a6c72c83d9fc1df15b703ed44cdf8e58
Jonathan Fine <jfine2358@gmail.com>
2016-03-07 11:14:07 +0000

    Add first version of a size function. Needs more tests, review.

--- py/linhomy2016/index.py
+++ py/linhomy2016/index.py
@@ -1,3 +1,26 @@
 '''The Index class (for use in linear homology)
 
 '''
+
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def size(rank, dim):
+    '''
+    >>> list(size(0, d) for d in range(8))
+    [1, 1, 2, 2, 3, 3, 4, 4]
+    '''
+
+    if rank * 3 > dim:
+        return 0
+
+    if rank == 0:
+        return dim // 2 + 1
+    else:
+
+        value = 0
+        for i in range(dim + 1):
+            j = dim - i - 3
+            value += size(0, i) * size(rank - 1, j)
+
+        return value
